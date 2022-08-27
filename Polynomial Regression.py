a37bdd7b31552591e15a7a17f706ecbac74c4e26
Gaurav Jain <gauravjain0781@gmail.com>
2022-08-27 21:53:45 +0530

    Created Polynomial Regressor

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -11,3 +11,14 @@ dataset = pd.read_csv("Position_Salaries.csv")
 X = dataset.iloc[:,1:2].values
 y = dataset.iloc[:,2].values
 
+# Building Linear Regressiion Model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X, y)
+
+# Building the Polynomial Regressor 
+from sklearn.preprocessing import PolynomialFeatures
+polynomial = PolynomialFeatures(degree=2)
+x_polynomial = polynomial.fit_transform(X)
+polynomial_regressor = LinearRegression()
+polynomial_regressor.fit(x_polynomial, y)
