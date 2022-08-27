7fea9514936bcebe2f80ad1140b40ac1c1934427
Gaurav Jain <gauravjain0781@gmail.com>
2022-08-27 22:47:49 +0530

    Visualized the results of Polynomial Linear Regressor

--- Polynomial Regression.py
+++ Polynomial Regression.py
@@ -19,14 +19,24 @@ y_predict = regressor.predict(X)
 
 # Building the Polynomial Regressor 
 from sklearn.preprocessing import PolynomialFeatures
-polynomial = PolynomialFeatures(degree=2)
+polynomial = PolynomialFeatures(degree=5)
 x_polynomial = polynomial.fit_transform(X)
 polynomial_regressor = LinearRegression()
 polynomial_regressor.fit(x_polynomial, y)
+y_polynomial_predict = polynomial_regressor.predict(x_polynomial)
+
 
 # Visualising results of Linear Regressor 
 plt.scatter(X, y, color="red")
 plt.plot(X, y_predict)
+plt.title("Salary VS Position")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
+plt.show()
+
+#Visualisind results of Polynomial Regressor 
+plt.scatter(X, y, color="red")
+plt.plot(X, y_polynomial_predict , color="blue")
 plt.title("Salary VS Position")
 plt.xlabel("Position Level")
 plt.ylabel("Salary")
